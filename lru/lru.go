879c4514a6e9106819072df1bc45b2c603931ffc
ziruih <ziruih73@gmail.com>
2021-07-05 18:59:27 +0800

    finish LRU

--- lru/lru.go
+++ lru/lru.go
@@ -32,3 +32,64 @@ func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
 		onEvicted: onEvicted,
 	}
 }
+
+// Get has two steps
+// 1.find element from dict
+// 2.move element to the end of queue (we assume Front is the end)
+func (c *Cache) Get(key string) (Value, bool) {
+	if ele, ok := c.cache[key]; ok {
+		// move ele to the end of queue
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry) // conversion: interface{} -> entry
+		return kv.value, true
+	}
+	return nil, false
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		// remove from ll
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		// delete from dict
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.onEvicted != nil {
+			c.onEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	// if key exist in cache, update the value and move the element to the end of queue.
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{
+			key:   key,
+			value: value,
+		})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	// if nBytes is excess maxBytes, remove the oldest element
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) GetNBytes() int64 {
+	return c.nbytes
+}
+
+func (c *Cache) GetMaxBytes() int64 {
+	return c.maxBytes
+}
+
+func (c *Cache) Len() int {
+	return len(c.cache)
+}
