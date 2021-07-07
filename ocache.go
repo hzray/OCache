2cd3909ea9448f68078cb33a9b1cf53d5dbfe435
ziruih <ziruih73@gmail.com>
2021-07-07 08:06:54 +0800

    concurrent operation

---
+++ ocache.go
@@ -0,0 +1,95 @@
+package ocache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+// A Getter loads data for a key
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// A GetterFunc implements Getter with a function.
+// 定义一个函数类型 F，并且实现接口 A 的方法，然后在这个方法中调用自己。
+// 这是 Go 语言中将其他函数（参数返回值定义与 F 一致）转换为接口 A 的常用技巧。
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implements Getter interface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+// A Group is a cache namespace and associated data loaded spread over
+type Group struct {
+	name      string // namespace
+	getter    Getter // 缓存未命中时获取源数据的回调(callback)
+	mainCache cache  //并发缓存
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// NewGroup create a new instance of Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+// GetGroup returns the named group previously created with NewGroup, or
+// nil if there's no such group.
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	g := groups[name]
+	return g
+}
+
+// Get value for a key from cache
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	// cache hit
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[oCache] hit")
+		return v, nil
+	}
+
+	// cache miss
+	return g.load(key)
+}
+
+// if cache miss, load from locally
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key) // get from source data
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	// add source data to main cache
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
