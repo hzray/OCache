343973a4f5df1dac68a832013d22d71624e12f73
ziruih <ziruih73@gmail.com>
2021-07-07 10:05:08 +0800

    consistent hash

---
+++ consistenthash/consistenthash.go
@@ -0,0 +1,64 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// Hash maps bytes to uint32
+type Hash func(data []byte) uint32
+
+// Map contains all hashed keys
+type Map struct {
+	hash     Hash
+	replicas int
+	keys     []int // Sorted
+	hashMap  map[int]string
+}
+
+// New creates a Map instance
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+// Add adds some keys to the hash.
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			// 对每一个真实节点 key，对应创建 m.replicas 个虚拟节点
+			// 通过添加编号的方式区分不同虚拟节点
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	// 环上的哈希值排序
+	sort.Ints(m.keys)
+}
+
+// Get gets the closest item in the hash to the provided key.
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+	// 计算 key 的哈希值
+	hash := int(m.hash([]byte(key)))
+	// Binary search for appropriate replica.
+	// 顺时针找到第一个匹配的虚拟节点的下标 idx
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	// 如果 idx == len(m.keys)，说明应选择 m.keys[0]，
+	// 因为 m.keys 是一个环状结构，所以用取余数的方式来处理这种情况。
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
